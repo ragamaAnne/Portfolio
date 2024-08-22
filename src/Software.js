8415e4c1cf17c8c682a4cc1056c553257506bb88
ragamaAnne <ragamaanne3@gmail.com>
2024-08-22 11:01:01 +0300

    'completed_s/w_page'

--- src/Software.js
+++ src/Software.js
@@ -8,24 +8,49 @@ function Software()  {
   return (
     <div class="software-main">
 
-        <h1 id='software-header'> Projects I have Done</h1>
+        <h1 align='center'> Projects I have worked on so far</h1>
 
         <div class="software-content">
 
           <div id="proj1">
-            <img id="proj1img"></img>
+
+            <div id='proj1img'>
+            
+            </div>
+            
+            <div id='info'>
+            <h2>Angaza</h2>
             <p>This is a website That is designed to assist Farmers market their products and access buyers fom their products</p>
             <p>I worked on this project using html css, javascript for front end and Php,Javascript and MySql for the backend</p>
+            <p><b>Github Repo Link-</b><a></a></p>
+            </div>
+            
           </div>
 
           <div id="proj2">
-            <img id="proj2img"></img>
-            <p id="proj2info"></p>
+            <div id='proj2img'>
+            
+            </div>
+            
+            <div id='info'>
+            <h2>Kodisha</h2>
+            <p>This is a website That is designed to assist students House hunt with ease</p>
+            <p>I worked on this project using .Net Framework for the front end I utilised c#,html and css </p>
+            <p><b>Github Repo Link-</b><a></a></p>
+            </div>
           </div>
 
           <div id="proj3">
-            <img id="proj3img"></img>
-            <p id="proj3info"></p>
+            <div id='proj3img'>
+            
+            </div>
+            
+            <div id='info'>
+            <h2>Water Delivery</h2>
+            <p>This is a mobile App that I created using Andriod studio to help in delivery of drinking water  to users door steps</p>
+            <p>I worked on this project using Java, css and Firebase for the database</p>
+            <p><b>Github Repo Link-</b><a></a></p>
+            </div>
           </div>
 
         </div>
